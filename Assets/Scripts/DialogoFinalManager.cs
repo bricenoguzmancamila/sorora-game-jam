40f5614ec26f51fa96347a891d6a4aa44e0e8b75
Camila Briceño Guzmán <bricenoguzmancamila@gmail.com>
2026-08-09 14:20:09 -0600

    recuperando cambios de dialogos

--- Assets/Scripts/DialogoFinalManager.cs
+++ Assets/Scripts/DialogoFinalManager.cs
@@ -1,36 +1,37 @@
 using UnityEngine;
 using TMPro;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
-public class DialogoSoloManager : MonoBehaviour{
+public class DialogoFinalManager : MonoBehaviour{
     public GameObject dialogo;
     public TMP_Text textoDialogo;
     public string[] lineas;
     public float delay=1f;
     public float velocidadLetra=0.05f;
+    public string siguienteEscena;
     private int lineaActual=-1;
+
     void Start(){
         dialogo.SetActive(false);
         Invoke(nameof(inicializacion),delay);
     }
     void Update(){
-        if (Input.GetKeyDown(KeyCode.Space) && lineaActual>=0){
+        if (Input.GetKeyDown(KeyCode.Space)&&lineaActual>=0){
             lineaSiguiente();
         }
     }
     void inicializacion(){
-        dialogo.SetActive(true);;
+        dialogo.SetActive(true);
         lineaActual=0;
         StartCoroutine(letraXLetra());
     }
-
     void lineaSiguiente(){
         ++lineaActual;
         if (lineaActual<lineas.Length){
             StartCoroutine(letraXLetra());
-        }
-        else{
-            dialogo.SetActive(false);
+        }else{
+            SceneManager.LoadScene(siguienteEscena);
         }
     }
     IEnumerator letraXLetra(){
