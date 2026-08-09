dd47da57bd4913fa7dfb05de5c159189a92c6357
Camila Briceño Guzmán <bricenoguzmancamila@gmail.com>
2026-08-09 00:31:03 -0600

    solo para ir a mimir

---
+++ Assets/Scripts/InterManager.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class InterManager : MonoBehaviour{
+    public GameObject sprite1;
+    public GameObject sprite2;
+    void Start(){
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
